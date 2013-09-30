3fc7fb6b9118a49b999617b40e1392be1b4d0135
Josh Bassett <josh.bassett@gmail.com>
2013-09-30 17:37:32 +1000

    Updated Main to assemble the given file.

--- src/Main.hs
+++ src/Main.hs
@@ -3,12 +3,14 @@ module Main where
 import Assembler
 import Parser
 
+import System.Environment
 import Text.ParserCombinators.Parsec
 
 main :: IO ()
 main = do
-  input <- getContents
-  case runParser instructions () "" input of
+  input <- getArgs
+  result <- parseFromFile instructions (head input)
+  case result of
     Left e -> print e
     Right instructions -> do
       let opcodes = runAssembler instructions
