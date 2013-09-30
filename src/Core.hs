b38db05327957cc0691e9a94324c3e73b93619ca
Josh Bassett <josh.bassett@gmail.com>
2013-09-30 20:47:48 +1000

    Added Directive data type.

--- src/Core.hs
+++ src/Core.hs
@@ -2,6 +2,8 @@ module Core where
 
 import Data.Word
 
+type Name = String
+
 -- A 12-bit program address.
 newtype Address = Address Word16 deriving (Eq, Show)
 
@@ -22,44 +24,60 @@ data Operand =
   | RegisterOperand Register
   deriving (Eq, Show)
 
+data Directives =
+    -- Assigns a name to a constant value. For example:
+    ConstantDirective Name Constant
+
+    -- Defines a list of characters for use with OUTPUTK and LOAD&RETURN
+    -- instructions.
+  | StringDirective Name String
+
+    -- Defines a list of values for use with OUTPUTK and LOAD&RETURN
+    -- instructions.
+  | TableDirective Name [Constant]
+
+    -- Inserts the contents of another file into the program immediately
+    -- following the directive.
+  | IncludeDirective FilePath
+
 data Instruction =
+    -- Loads a value into the first operand from the second operand.
     -- LOAD sX, sY
     -- LOAD sX, kk
-    -- Loads a value into the first operand from the second operand.
     LoadInstruction Operand Operand
 
+    -- Performs a bit-wise logical AND operation on the first and second operands.
     -- AND sX, sY
     -- AND sX, kk
-    -- Performs a bit-wise logical AND operation on the first and second operands.
   | AndInstruction Operand Operand
 
+    -- Performs a bit-wise logical OR operation on the first and second operands.
     -- OR sX, sY
     -- OR sX, kk
-    -- Performs a bit-wise logical OR operation on the first and second operands.
   | OrInstruction Operand Operand
 
+    -- Performs a bit-wise logical XOR operation on the first and second operands.
     -- XOR sX, sY
     -- XOR sX, kk
-    -- Performs a bit-wise logical XOR operation on the first and second operands.
   | XorInstruction Operand Operand
 
-    -- SL0 sX
     -- Performs a shift left operation on the operand.
+    -- SL0 sX
   | ShiftLeft0Instruction Operand
 
-    -- SL1 sX
     -- Performs a shift left operation on the operand.
+    -- SL1 sX
   | ShiftLeft1Instruction Operand
 
-    -- JUMP aaa
     -- Jumps to an address.
+    -- JUMP aaa
   | JumpInstruction Operand
 
-    -- CALL aaa
     -- Calls a subroutine at an address.
+    -- CALL aaa
   | CallInstruction Operand
 
-    -- RETURN
     -- Completes a subroutine.
+    -- RETURN
   | ReturnInstruction
   deriving (Eq, Show)
