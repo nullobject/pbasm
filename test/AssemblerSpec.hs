9cfe4e89a0b258df26b9524029a65c12eef88d24
Josh Bassett <josh.bassett@gmail.com>
2013-10-07 11:39:50 +1100

    Refactored the Assembler to use a Reader monad.

--- test/AssemblerSpec.hs
+++ test/AssemblerSpec.hs
@@ -3,6 +3,7 @@ module AssemblerSpec where
 import Assembler
 import Core
 
+import Control.Monad.Reader
 import Data.Map (fromList)
 import Test.Hspec
 import Text.ParserCombinators.Parsec
@@ -16,49 +17,49 @@ spec = do
     let map = fromList [("foo", Address 1), ("bar", Address 2)]
 
     it "assembles a 'LOAD sX, sY' instruction" $ do
-      let result = assemble (BinaryInstruction "load" (RegisterOperand Register0) (RegisterOperand Register1)) map
+      let result = runReader (assemble (BinaryInstruction "load" (RegisterOperand Register0) (RegisterOperand Register1))) map
       result `shouldBe` 0x00010
 
     it "assembles a 'LOAD sX, kk' instruction" $ do
-      let result = assemble (BinaryInstruction "load" (RegisterOperand Register0) (ConstantOperand 0xFF)) map
+      let result = runReader (assemble (BinaryInstruction "load" (RegisterOperand Register0) (ConstantOperand 0xFF))) map
       result `shouldBe` 0x010FF
 
     it "assembles a 'AND sX, sY' instruction" $ do
-      let result = assemble (BinaryInstruction "and" (RegisterOperand Register0) (RegisterOperand Register1)) map
+      let result = runReader (assemble (BinaryInstruction "and" (RegisterOperand Register0) (RegisterOperand Register1))) map
       result `shouldBe` 0x02010
 
     it "assembles a 'AND sX, kk' instruction" $ do
-      let result = assemble (BinaryInstruction "and" (RegisterOperand Register0) (ConstantOperand 0xFF)) map
+      let result = runReader (assemble (BinaryInstruction "and" (RegisterOperand Register0) (ConstantOperand 0xFF))) map
       result `shouldBe` 0x030FF
 
     it "assembles a 'OR sX, sY' instruction" $ do
-      let result = assemble (BinaryInstruction "or" (RegisterOperand Register0) (RegisterOperand Register1)) map
+      let result = runReader (assemble (BinaryInstruction "or" (RegisterOperand Register0) (RegisterOperand Register1))) map
       result `shouldBe` 0x04010
 
     it "assembles a 'OR sX, kk' instruction" $ do
-      let result = assemble (BinaryInstruction "or" (RegisterOperand Register0) (ConstantOperand 0xFF)) map
+      let result = runReader (assemble (BinaryInstruction "or" (RegisterOperand Register0) (ConstantOperand 0xFF))) map
       result `shouldBe` 0x050FF
 
     it "assembles a 'XOR sX, sY' instruction" $ do
-      let result = assemble (BinaryInstruction "xor" (RegisterOperand Register0) (RegisterOperand Register1)) map
+      let result = runReader (assemble (BinaryInstruction "xor" (RegisterOperand Register0) (RegisterOperand Register1))) map
       result `shouldBe` 0x06010
 
     it "assembles a 'XOR sX, kk' instruction" $ do
-      let result = assemble (BinaryInstruction "xor" (RegisterOperand Register0) (ConstantOperand 0xFF)) map
+      let result = runReader (assemble (BinaryInstruction "xor" (RegisterOperand Register0) (ConstantOperand 0xFF))) map
       result `shouldBe` 0x070FF
 
     it "assembles a 'SL0 sX' instruction" $ do
-      let result = assemble (UnaryInstruction "sl0" (RegisterOperand Register0)) map
+      let result = runReader (assemble (UnaryInstruction "sl0" (RegisterOperand Register0))) map
       result `shouldBe` 0x14006
 
     it "assembles a 'SL1 sX' instruction" $ do
-      let result = assemble (UnaryInstruction "sl1" (RegisterOperand Register0)) map
+      let result = runReader (assemble (UnaryInstruction "sl1" (RegisterOperand Register0))) map
       result `shouldBe` 0x14007
 
     it "assembles a 'CALL aaa' instruction" $ do
-      let result = assemble (UnaryInstruction "call" (AddressOperand 0xFFF)) map
+      let result = runReader (assemble (UnaryInstruction "call" (AddressOperand 0xFFF))) map
       result `shouldBe` 0x20FFF
 
     it "assembles a 'CALL label' instruction" $ do
-      let result = assemble (UnaryInstruction "call" (LabelOperand "foo")) map
+      let result = runReader (assemble (UnaryInstruction "call" (LabelOperand "foo"))) map
       result `shouldBe` 0x20001
