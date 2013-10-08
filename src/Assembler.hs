975bd5cb8190fe49aedd6518b0d6b13a6e88ab55
Josh Bassett <josh.bassett@gmail.com>
2013-10-08 17:25:31 +1100

    Handle arithmetic instructions in the assembler.

--- src/Assembler.hs
+++ src/Assembler.hs
@@ -53,21 +53,46 @@ pack op _ _ = return $ Just $ op `shiftL` 12
 -- Assembles the given statement.
 assemble :: Statement -> AssemblerReader (Maybe Word32)
 
+-- Register loading
 assemble (BinaryInstruction "load" x y@(RegisterOperand _)) = pack 0x00 x y
-assemble (BinaryInstruction "load" x y                    ) = pack 0x01 x y
-assemble (BinaryInstruction "and"  x y@(RegisterOperand _)) = pack 0x02 x y
-assemble (BinaryInstruction "and"  x y                    ) = pack 0x03 x y
-assemble (BinaryInstruction "or"   x y@(RegisterOperand _)) = pack 0x04 x y
-assemble (BinaryInstruction "or"   x y                    ) = pack 0x05 x y
-assemble (BinaryInstruction "xor"  x y@(RegisterOperand _)) = pack 0x06 x y
-assemble (BinaryInstruction "xor"  x y                    ) = pack 0x07 x y
+assemble (BinaryInstruction "load" x y)                     = pack 0x01 x y
 
-assemble (UnaryInstruction "sl0"  x) = pack 0x14 x (DataOperand 0x06)
-assemble (UnaryInstruction "sl1"  x) = pack 0x14 x (DataOperand 0x07)
-assemble (UnaryInstruction "call" x) = pack 0x20 x (DataOperand 0x00)
+-- Logical
+assemble (BinaryInstruction "and" x y@(RegisterOperand _)) = pack 0x02 x y
+assemble (BinaryInstruction "and" x y)                     = pack 0x03 x y
+assemble (BinaryInstruction "or"  x y@(RegisterOperand _)) = pack 0x04 x y
+assemble (BinaryInstruction "or"  x y)                     = pack 0x05 x y
+assemble (BinaryInstruction "xor" x y@(RegisterOperand _)) = pack 0x06 x y
+assemble (BinaryInstruction "xor" x y)                     = pack 0x07 x y
 
+-- Arithmetic
+assemble (BinaryInstruction "add"   x y@(RegisterOperand _)) = pack 0x10 x y
+assemble (BinaryInstruction "add"   x y)                     = pack 0x11 x y
+assemble (BinaryInstruction "addcy" x y@(RegisterOperand _)) = pack 0x12 x y
+assemble (BinaryInstruction "addcy" x y)                     = pack 0x13 x y
+assemble (BinaryInstruction "sub"   x y@(RegisterOperand _)) = pack 0x18 x y
+assemble (BinaryInstruction "sub"   x y)                     = pack 0x19 x y
+assemble (BinaryInstruction "subcy" x y@(RegisterOperand _)) = pack 0x1A x y
+assemble (BinaryInstruction "subcy" x y)                     = pack 0x1B x y
+
+-- Test and compare
+-- TODO
+
+-- IO
+-- TODO
+
+-- Shift and rotate
+assemble (UnaryInstruction "sl0" x) = pack 0x14 x (DataOperand 0x06)
+assemble (UnaryInstruction "sl1" x) = pack 0x14 x (DataOperand 0x07)
+
+-- Jump
+-- TODO
+
+-- Subroutines
+assemble (UnaryInstruction "call" x)   = pack 0x20 x (DataOperand 0x00)
 assemble (NullaryInstruction "return") = pack 0x25 (DataOperand 0x00) (DataOperand 0x00)
 
+-- Default
 assemble _ = return Nothing
 
 -- Assembles the given statements into binary format.
--- test/AssemblerSpec.hs
+++ test/AssemblerSpec.hs
@@ -14,72 +14,75 @@ spec :: Spec
 spec = do
   describe "runAssembler" $ do
     it "assembles a 'LOAD sX, sY' instruction" $ do
-      run (BinaryInstruction "load" (RegisterOperand Register0) (RegisterOperand Register1))
-        `shouldBe` [0x00010]
+      run (BinaryInstruction "load" (RegisterOperand Register0) (RegisterOperand Register1)) `shouldBe` [0x00010]
 
-    it "assembles a 'LOAD sX, kk' instruction with a data value" $ do
-      run (BinaryInstruction "load" (RegisterOperand Register0) (DataOperand 0xFF))
-        `shouldBe` [0x010FF]
-
-    it "assembles a 'LOAD sX, kk' instruction with a constant value" $ do
-      run (BinaryInstruction "load" (RegisterOperand Register0) (IdentifierOperand "lorem"))
-        `shouldBe` [0x01001]
+    it "assembles a 'LOAD sX, kk' instruction" $ do
+      run (BinaryInstruction "load" (RegisterOperand Register0) (DataOperand 0xFF)) `shouldBe` [0x010FF]
+      run (BinaryInstruction "load" (RegisterOperand Register0) (IdentifierOperand "lorem")) `shouldBe` [0x01001]
 
     it "assembles a 'AND sX, sY' instruction" $ do
-      run (BinaryInstruction "and" (RegisterOperand Register0) (RegisterOperand Register1))
-        `shouldBe` [0x02010]
+      run (BinaryInstruction "and" (RegisterOperand Register0) (RegisterOperand Register1)) `shouldBe` [0x02010]
 
-    it "assembles a 'AND sX, kk' instruction with a data value" $ do
-      run (BinaryInstruction "and" (RegisterOperand Register0) (DataOperand 0xFF))
-        `shouldBe` [0x030FF]
-
-    it "assembles a 'AND sX, kk' instruction with a constant value" $ do
-      run (BinaryInstruction "and" (RegisterOperand Register0) (IdentifierOperand "lorem"))
-        `shouldBe` [0x03001]
+    it "assembles a 'AND sX, kk' instruction" $ do
+      run (BinaryInstruction "and" (RegisterOperand Register0) (DataOperand 0xFF)) `shouldBe` [0x030FF]
+      run (BinaryInstruction "and" (RegisterOperand Register0) (IdentifierOperand "lorem")) `shouldBe` [0x03001]
 
     it "assembles a 'OR sX, sY' instruction" $ do
-      run (BinaryInstruction "or" (RegisterOperand Register0) (RegisterOperand Register1))
-        `shouldBe` [0x04010]
+      run (BinaryInstruction "or" (RegisterOperand Register0) (RegisterOperand Register1)) `shouldBe` [0x04010]
 
-    it "assembles a 'OR sX, kk' instruction with a data value" $ do
-      run (BinaryInstruction "or" (RegisterOperand Register0) (DataOperand 0xFF))
-        `shouldBe` [0x050FF]
-
-    it "assembles a 'OR sX, kk' instruction with a constant value" $ do
-      run (BinaryInstruction "or" (RegisterOperand Register0) (IdentifierOperand "lorem"))
-        `shouldBe` [0x05001]
+    it "assembles a 'OR sX, kk' instruction" $ do
+      run (BinaryInstruction "or" (RegisterOperand Register0) (DataOperand 0xFF)) `shouldBe` [0x050FF]
+      run (BinaryInstruction "or" (RegisterOperand Register0) (IdentifierOperand "lorem")) `shouldBe` [0x05001]
 
     it "assembles a 'XOR sX, sY' instruction" $ do
-      run (BinaryInstruction "xor" (RegisterOperand Register0) (RegisterOperand Register1))
-        `shouldBe` [0x06010]
+      run (BinaryInstruction "xor" (RegisterOperand Register0) (RegisterOperand Register1)) `shouldBe` [0x06010]
 
-    it "assembles a 'XOR sX, kk' instruction with a data value" $ do
-      run (BinaryInstruction "xor" (RegisterOperand Register0) (DataOperand 0xFF))
-        `shouldBe` [0x070FF]
+    it "assembles a 'XOR sX, kk' instruction" $ do
+      run (BinaryInstruction "xor" (RegisterOperand Register0) (DataOperand 0xFF)) `shouldBe` [0x070FF]
+      run (BinaryInstruction "xor" (RegisterOperand Register0) (IdentifierOperand "lorem")) `shouldBe` [0x07001]
 
-    it "assembles a 'XOR sX, kk' instruction with a constant value" $ do
-      run (BinaryInstruction "xor" (RegisterOperand Register0) (IdentifierOperand "lorem"))
-        `shouldBe` [0x07001]
+    it "assembles a 'ADD sX, sY' instruction" $ do
+      run (BinaryInstruction "add" (RegisterOperand Register0) (RegisterOperand Register1)) `shouldBe` [0x10010]
+
+    it "assembles a 'ADD sX, kk' instruction" $ do
+      run (BinaryInstruction "add" (RegisterOperand Register0) (DataOperand 0xFF)) `shouldBe` [0x110FF]
+      run (BinaryInstruction "add" (RegisterOperand Register0) (IdentifierOperand "lorem")) `shouldBe` [0x11001]
+
+    it "assembles a 'ADDCY sX, sY' instruction" $ do
+      run (BinaryInstruction "addcy" (RegisterOperand Register0) (RegisterOperand Register1)) `shouldBe` [0x12010]
+
+    it "assembles a 'ADDCY sX, kk' instruction" $ do
+      run (BinaryInstruction "addcy" (RegisterOperand Register0) (DataOperand 0xFF)) `shouldBe` [0x130FF]
+      run (BinaryInstruction "addcy" (RegisterOperand Register0) (IdentifierOperand "lorem")) `shouldBe` [0x13001]
+
+    it "assembles a 'SUB sX, sY' instruction" $ do
+      run (BinaryInstruction "sub" (RegisterOperand Register0) (RegisterOperand Register1)) `shouldBe` [0x18010]
+
+    it "assembles a 'SUB sX, kk' instruction" $ do
+      run (BinaryInstruction "sub" (RegisterOperand Register0) (DataOperand 0xFF)) `shouldBe` [0x190FF]
+      run (BinaryInstruction "sub" (RegisterOperand Register0) (IdentifierOperand "lorem")) `shouldBe` [0x19001]
+
+    it "assembles a 'SUBCY sX, sY' instruction" $ do
+      run (BinaryInstruction "subcy" (RegisterOperand Register0) (RegisterOperand Register1)) `shouldBe` [0x1A010]
+
+    it "assembles a 'SUBCY sX, kk' instruction" $ do
+      run (BinaryInstruction "subcy" (RegisterOperand Register0) (DataOperand 0xFF)) `shouldBe` [0x1B0FF]
+      run (BinaryInstruction "subcy" (RegisterOperand Register0) (IdentifierOperand "lorem")) `shouldBe` [0x1B001]
 
     it "assembles a 'SL0 sX' instruction" $ do
-      run (UnaryInstruction "sl0" (RegisterOperand Register0))
-        `shouldBe` [0x14006]
+      run (UnaryInstruction "sl0" (RegisterOperand Register0)) `shouldBe` [0x14006]
 
     it "assembles a 'SL1 sX' instruction" $ do
-      run (UnaryInstruction "sl1" (RegisterOperand Register0))
-        `shouldBe` [0x14007]
+      run (UnaryInstruction "sl1" (RegisterOperand Register0)) `shouldBe` [0x14007]
 
     it "assembles a 'CALL aaa' instruction" $ do
-      run (UnaryInstruction "call" (AddressOperand 0xFFF))
-        `shouldBe` [0x20FFF]
+      run (UnaryInstruction "call" (AddressOperand 0xFFF)) `shouldBe` [0x20FFF]
 
     it "assembles a 'CALL aaa' instruction with a label" $ do
-      run (UnaryInstruction "call" (IdentifierOperand "foo"))
-        `shouldBe` [0x20001]
+      run (UnaryInstruction "call" (IdentifierOperand "foo")) `shouldBe` [0x20001]
 
     it "assembles a 'RETURN' instruction" $ do
-      run (NullaryInstruction "return")
-        `shouldBe` [0x25000]
+      run (NullaryInstruction "return") `shouldBe` [0x25000]
 
     where
       run i       = runAssembler [i] constantMap labelMap
